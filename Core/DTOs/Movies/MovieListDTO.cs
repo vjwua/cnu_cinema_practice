031db38ffbc1c3c5af63a058f9e935abf64dfbcd
DubetsIvan <158566315+DubetsIvan@users.noreply.github.com>
2026-01-20 15:15:32 +0200

    feat: add Movie DTOs

--- Core/DTOs/Movies/MovieListDTO.cs
+++ Core/DTOs/Movies/MovieListDTO.cs
@@ -1,6 +1,14 @@
-namespace Core.DTOs;
+using Core.Enums;
+
+namespace Core.DTOs.Movies;
 
 public class MovieListDTO
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string? PosterUrl { get; set; }
+    public decimal? ImdbRating { get; set; }
+    public DateOnly ReleaseDate { get; set; }
+    public MovieGenre Genre { get; set; }
+    public ushort DurationMinutes { get; set; }
 }
