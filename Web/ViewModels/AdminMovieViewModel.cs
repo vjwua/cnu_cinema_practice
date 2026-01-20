aa888a259cb6eb4d1f3293ee6f4af0a21362b5d7
vjwua <62188903+vjwua@users.noreply.github.com>
2026-01-20 22:32:14 +0200

    Added admin panel

---
+++ Web/ViewModels/AdminMovieViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace cnu_cinema_practice.ViewModels
+{
+    public class AdminMovieViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PosterUrl { get; set; }
+        public int DurationMinutes { get; set; }
+        public string ImdbRating { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Director { get; set; }
+        public string Description { get; set; }
+        public List<string> Genres { get; set; }
+        public bool IsActive { get; set; }
+    }
+
+    public class MovieFormViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PosterUrl { get; set; }
+        public int DurationMinutes { get; set; }
+        public string ImdbRating { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Director { get; set; }
+        public string Description { get; set; }
+        public string GenresString { get; set; } // Comma-separated
+        public bool IsActive { get; set; }
+    }
+}
