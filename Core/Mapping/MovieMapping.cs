0bb0dcb7181c2eb8849c01ea930c70fa80b04eb8
DubetsIvan <158566315+DubetsIvan@users.noreply.github.com>
2026-01-21 14:53:13 +0200

    feat: add mapper for Movie

---
+++ Core/Mapping/MovieMapping.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Core.Entities;
+using Core.DTOs.Movies;
+
+namespace Core.Mapping;
+
+public class MovieMapping : Profile
+{
+    public MovieMapping()
+    {
+        CreateMap<Movie, MovieListDTO>();
+        
+        CreateMap<Movie, MovieDetailDTO>()
+            .ForMember(x => x.Actors, opt => opt.MapFrom(x => x.Actors))
+            .ForMember(x => x.Directors, opt => opt.MapFrom(x => x.Directors));
+
+        CreateMap<Person, MoviePersonDTO>();
+
+        CreateMap<CreateMovieDTO, Movie>()
+            .ForMember(x => x.Actors, x => x.Ignore())
+            .ForMember(x => x.Directors, x => x.Ignore());
+
+        CreateMap<UpdateMovieDTO, Movie>()
+            .ForMember(x => x.Actors, x => x.Ignore())
+            .ForMember(x => x.Directors, x => x.Ignore())
+            .ForMember(x => x.Sessions, x => x.Ignore());
+    }
+}
