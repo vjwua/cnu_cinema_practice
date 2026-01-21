1f4f8c9f05c4c7aff7d738f897d17b4cbdda52fc
DubetsIvan <158566315+DubetsIvan@users.noreply.github.com>
2026-01-21 16:23:56 +0200

    feat: add IMovieService

---
+++ Core/Interfaces/Services/IMovieService.cs
@@ -0,0 +1,16 @@
+﻿using Core.DTOs.Movies;
+using Core.Entities;
+using Core.Enums;
+
+namespace Core.Interfaces.Services;
+
+public interface IMovieService
+{
+    Task<IEnumerable<MovieListDTO>> GetAllAsync();
+    Task<MovieDetailDTO?> GetByIdAsync(int id);
+    Task<IEnumerable<MovieDetailDTO>> GetByGenreAsync(MovieGenre genre);
+    Task<MovieDetailDTO> CreateAsync(Movie movie);
+    Task UpdateAsync(Movie movie);
+    Task DeleteAsync(Movie movie);
+    Task<IEnumerable<MovieListDTO>> SearchByNameAsync(string searchTerm);
+}
