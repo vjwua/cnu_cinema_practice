133ff0c14fd52890c24883dd463b7d2fcf4dbf5f
Viacheslav Rudyi <rudyi.viacheslav@comp-sc.if.ua>
2026-01-22 16:26:17 +0200

    Hall DTO validators

--- Core/DTOs/Halls/CreateHallDTO.cs
+++ Core/DTOs/Halls/CreateHallDTO.cs
@@ -4,7 +4,7 @@ namespace Core.DTOs.Halls;
 
 public class CreateHallDTO
 {
-    public int Name { get; set; }
+    public string Name { get; set; }
     public byte Rows { get; set; }
     public byte Columns { get; set; }
     public byte[,] SeatLayout { get; set; } = null!;
--- Core/Validators/Halls/CreateHallDTOValidator.cs
+++ Core/Validators/Halls/CreateHallDTOValidator.cs
@@ -1,6 +1,34 @@
+using FluentValidation;
+using Core.DTOs.Halls;
+
 namespace Core.Validators.Hall;
 
-public class CreateHallDTOValidator
+public class CreateHallDTOValidator : AbstractValidator<CreateHallDTO>
 {
-    
+    public CreateHallDTOValidator()
+    {
+        RuleFor(h => h.Name)
+            .NotEmpty().WithMessage("Hall name is required")
+            .MaximumLength(100).WithMessage("Hall name cannot exceed 100 characters");
+        RuleFor(h => h.Columns)
+            .InclusiveBetween((byte) 1, (byte) 50)
+            .WithMessage("Hall must contain between 1 and 50 columns");
+        RuleFor(h => h.Rows)
+            .InclusiveBetween((byte) 1, (byte) 50)
+            .WithMessage("Hall must contain between 1 and 50 rows");
+        RuleFor(h => h)
+            .Must(DimensionsAlign)
+            .WithMessage("Layout dimensions must align with Rows and Columns values");
+    }
+
+    private bool DimensionsAlign(CreateHallDTO dto)
+    {
+        if (dto.SeatLayout.GetLength(0) == dto.Rows
+            && dto.SeatLayout.GetLength(1) == dto.Columns)
+        {
+            return true;
+        }
+
+        return false;
+    } 
 }
