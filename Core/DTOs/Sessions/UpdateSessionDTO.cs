7c08b6825ce134bed9ea04630be1d34e90887e6b
Krupka-Yuliia <yuliia.krupka.22@pnu.edu.ua>
2026-01-23 19:12:19 +0200

    fix update session method

--- Core/DTOs/Sessions/UpdateSessionDTO.cs
+++ Core/DTOs/Sessions/UpdateSessionDTO.cs
@@ -4,8 +4,6 @@ namespace Core.DTOs.Sessions;
 
 public class UpdateSessionDTO
 {
-    //це id сутності для оновлення
-    public int Id { get; set; }
     public int? MovieId { get; set; }
     public int? HallId { get; set; }
     public DateTime? StartTime { get; set; }
