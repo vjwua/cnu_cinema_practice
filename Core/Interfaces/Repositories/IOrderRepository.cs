cfa548b66fab023b00f1bbb8f77253931e0f2936
1vanytska <sofiia.ivanytska.22@pnu.edu.ua>
2026-01-24 22:01:00 +0200

    Resolve missing view error in Admin area

---
+++ Core/Interfaces/Repositories/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+using Core.Enums;
+
+namespace Core.Interfaces.Repositories;
+
+public interface IOrderRepository
+{
+    Task<Order?> GetByIdAsync(int id);
+    Task<IEnumerable<Order>> GetByUserIdAsync(string userId);
+    Task<Order> CreateAsync(Order order);
+    Task UpdateAsync(Order order);
+    Task<IEnumerable<Order>> GetExpiredPendingOrdersAsync(DateTime cutoffTime);
+    Task<IEnumerable<Order>> GetOrdersBySessionIdAsync(int sessionId);
+}
