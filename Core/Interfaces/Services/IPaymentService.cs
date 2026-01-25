a97ba3020e6e6dcf1bf5589a2533585af380504c
1vanytska <sofiia.ivanytska.22@pnu.edu.ua>
2026-01-25 18:41:24 +0200

    implement OrderService and PaymentService with transaction support

---
+++ Core/Interfaces/Services/IPaymentService.cs
@@ -0,0 +1,8 @@
+﻿using Core.DTOs.Payments;
+
+namespace Core.Interfaces.Services;
+
+public interface IPaymentService
+{
+    Task<PaymentDTO> ProcessPaymentAsync(CreatePaymentDTO dto);
+}
---
+++ Infrastructure/Repositories/SeatReservationRepository.cs
@@ -0,0 +1,25 @@
+﻿using Core.Entities;
+using Core.Enums;
+using Core.Interfaces.Repositories;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class SeatReservationRepository(CinemaDbContext context) : ISeatReservationRepository
+{
+    public async Task<List<SeatReservation>> GetByIdsAsync(List<int> ids)
+    {
+        return await context.SeatReservations
+            .Include(sr => sr.Seat)
+            .Where(sr => ids.Contains(sr.Id))
+            .ToListAsync();
+    }
+
+    public async Task MarkAsSoldAsync(List<int> ids)
+    {
+        await context.SeatReservations
+            .Where(sr => ids.Contains(sr.Id))
+            .ExecuteUpdateAsync(s => s.SetProperty(r => r.Status, ReservationStatus.Sold));
+    }
+}
