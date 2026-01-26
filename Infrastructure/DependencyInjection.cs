e6794b7c7b7c803384a19dc43f2968b3360b2ba8
Krupka-Yuliia <yuliia.krupka.22@pnu.edu.ua>
2026-01-26 18:19:36 +0200

    fix project structure to ensure pages for sessions are displayed

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -1,9 +1,21 @@
 using Infrastructure.Identity;
 using Infrastructure.Data;
+using Infrastructure.Repositories;
+using Core.Interfaces.Repositories;
+using Core.Interfaces.Services;
+using Core.Services;
+using Core.Validators.Sessions;
+using Core.Validators.Movies;
+using Core.Validators.Hall;
+using Core.DTOs.Sessions;
+using Core.DTOs.Movies;
+using Core.DTOs.Halls;
+using Core.Validators.Hall;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using FluentValidation;
 
 namespace Infrastructure;
 
@@ -21,6 +33,34 @@ public static class DependencyInjection
         services.AddIdentity<ApplicationUser, IdentityRole>()
             .AddEntityFrameworkStores<CinemaDbContext>()
             .AddDefaultTokenProviders();
+
+        // Register Repositories
+        services.AddScoped<ISessionRepository, SessionRepository>();
+        services.AddScoped<IMovieRepository, MovieRepository>();
+        services.AddScoped<IHallRepository, HallRepository>();
+        services.AddScoped<ISeatRepository, SeatRepository>();
+
+        // Register Services
+        services.AddScoped<ISessionService, SessionService>();
+        services.AddScoped<IMovieService, MovieService>();
+        services.AddScoped<IHallService, HallService>();
+        services.AddScoped<ISeatService, SeatService>();
+
+        // Register Validators
+        // Session Validators
+        services.AddScoped<IValidator<CreateSessionDTO>, CreateSessionDtoValidator>();
+        services.AddScoped<IValidator<UpdateSessionDTO>, UpdateSessionDtoValidator>();
+        services.AddScoped<SessionBusinessValidator>();
+        services.AddScoped<CreateSessionBusinessValidator>();
+        services.AddScoped<UpdateSessionBusinessValidator>();
+
+        // Movie Validators
+        services.AddScoped<IValidator<CreateMovieDTO>, CreateMovieDTOValidator>();
+        services.AddScoped<IValidator<UpdateMovieDTO>, UpdateMovieDTOValidator>();
+
+        // Hall Validators
+        services.AddScoped<IValidator<CreateHallDTO>, CreateHallDTOValidator>();
+        services.AddScoped<IValidator<UpdateHallDTO>, UpdateHallDTOValidator>();
 
         return services;
     }
--- Infrastructure/Repositories/SeatRepository.cs
+++ Infrastructure/Repositories/SeatRepository.cs
@@ -4,17 +4,17 @@ using Core.Interfaces.Repositories;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
-namespace Infrastructure.Repositories.Interfaces;
+namespace Infrastructure.Repositories;
 
 public class SeatRepository : ISeatRepository
 {
     private CinemaDbContext _context;
-    private readonly HallRepository _hallRepo;
+    private readonly IHallRepository _hallRepo;
     private readonly DbSet<Seat> _seats;
     private readonly DbSet<Session> _sessions;
     private readonly DbSet<SeatReservation> _seatReservations;
 
-    public SeatRepository(CinemaDbContext con, HallRepository hRepo)
+    public SeatRepository(CinemaDbContext con, IHallRepository hRepo)
     {
         this._context = con;
         this._seats = this._context.Seats;
