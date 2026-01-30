e19601b93dd22dc187e707f515f0fe3d2118e773
vjwua <62188903+vjwua@users.noreply.github.com>
2026-01-30 21:38:30 +0200

    Admin renamed to Dashboard

---
+++ Web/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cnu_cinema_practice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
